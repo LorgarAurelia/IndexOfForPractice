5da7103ef7585b0a67eb5b3b4a2794bb942e2b3f
David Maltsev <luterfaeron@gmail.com>
2021-07-27 16:36:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace IndexOfForPractice
+{
+    class Program
+    {
+        static int IndexOf(int[] array, string valueForSearch) 
+        {
+            int parsedValue;
+
+            try
+            {
+                parsedValue = int.Parse(valueForSearch);
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+
+            for (int i = 0; i < array.Length; i++) 
+            {
+                if (array[i] == parsedValue)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
